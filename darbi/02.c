06c72624a30e6893898267b7f8fbbe7a163ae6e4
user <mjushenkov@gmail.com>
2019-09-23 18:09:57 +0300

    history333333

---
+++ darbi/02.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+// Hello World! // neder
+// "Hello World!" // neder
+// "Hello World!"; // der. bet teksts neparādās
+// printf("Hello World!"); // der, bet nav smuki
+printf("Hello World!\r\t\a\'\"\?\\\n");
+
+printf("Hello World!\n");
+
+
+}
