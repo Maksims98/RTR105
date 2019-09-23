d6ef8106631f556904b81c74589524aed8d68c19
user <mjushenkov@gmail.com>
2019-09-23 16:09:08 +0300

    history_20190923_16_07

---
+++ darbi/elementary.c
@@ -0,0 +1,6 @@
+// Komentārs - 1 rinda
+void main(){}
+/* Komentara bloks
+turpinājums
+beigas */
+
---
+++ darbi/hello.c
@@ -0,0 +1,9 @@
+void main()
+{
+// Hello World! // neder
+// "Hello World!" // neder
+// "Hello World!"; // der. bet teksts neparādās
+// printf("Hello World!"); // der, bet nav smuki
+printf("Hello World!/n");
+}
+
